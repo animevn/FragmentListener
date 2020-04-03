54f05446d66d8efe71ce1a74067667e0ebae95be
animevn <haanhth21@gmail.com>
2020-04-03 08:45:44 +0700

    init layout

--- app/src/main/java/com/haanhgs/app/fragmentlistener/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/fragmentlistener/MainActivity.java
@@ -1,16 +1,26 @@
 package com.haanhgs.app.fragmentlistener;
 
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
 
+    @BindView(R.id.bnOpen)
+    Button bnOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.bnOpen)
+    public void onViewClicked() {
+    }
 }
