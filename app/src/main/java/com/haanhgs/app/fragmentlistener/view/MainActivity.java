4bf55e90c97aa5cb0c38644a1faeb37310cbbc7f
saigonjew <saigonjew@gmail.com>
2020-09-29 07:07:56 +0700

    reset

--- app/src/main/java/com/haanhgs/app/fragmentlistener/view/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/fragmentlistener/view/MainActivity.java
@@ -1,101 +1,19 @@
 package com.haanhgs.app.fragmentlistener.view;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Surface;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
-import android.view.WindowManager;
-import android.widget.Button;
 import com.haanhgs.app.fragmentlistener.R;
-import com.haanhgs.app.fragmentlistener.viewmodel.MyViewModel;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity{
-
-    @BindView(R.id.bnOpen)
-    Button bnOpen;
-
-    private MyViewModel viewModel;
-    private boolean isOpen;
-
-
-    private void initViewModel(){
-        viewModel = new ViewModelProvider(this).get(MyViewModel.class);
-        viewModel.getData().observe(this, model -> {
-            isOpen = model.isOpen();
-            if (model.isOpen()){
-                bnOpen.setText(getResources().getString(R.string.close));
-            }else {
-                bnOpen.setText(getResources().getString(R.string.open));
-            }
-        });
-    }
-
-    @SuppressWarnings("deprecation")
-    private void setFullScreenInLandscape(){
-        if (getDisplay() != null){
-            int rotation = getDisplay().getRotation();
-            if (rotation == Surface.ROTATION_270 || rotation == Surface.ROTATION_90){
-                if (getSupportActionBar() != null) getSupportActionBar().hide();
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
-                    final WindowInsetsController controller = getWindow().getInsetsController();
-                    if (controller != null){
-                        controller.hide(WindowInsets.Type.statusBars());
-                    }
-                }else {
-                    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
-                }
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        setFullScreenInLandscape();
-        initViewModel();
-    }
-
-    private void openFragment(){
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag("fragment");
-        if (fragment == null){
-            SimpleFragment simpleFragment = new SimpleFragment();
-            ft.add(R.id.flFragment, simpleFragment, "fragment");
-            ft.commit();
-        }else {
-            ft.attach(fragment);
-        }
-    }
-
-    private void closeFragment(){
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag("fragment");
-        if (fragment != null){
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.remove(fragment);
-            ft.commit();
-        }
-    }
 
 
-    @OnClick(R.id.bnOpen)
-    public void onViewClicked() {
-        if (isOpen){
-            closeFragment();
-        }else {
-            openFragment();
-        }
-        viewModel.toggleFragment();
+
     }
+
 
 }
--- app/src/main/java/com/haanhgs/app/fragmentlistener/viewmodel/MyViewModel.java
+++ app/src/main/java/com/haanhgs/app/fragmentlistener/viewmodel/MyViewModel.java
@@ -1,32 +1,9 @@
 package com.haanhgs.app.fragmentlistener.viewmodel;
 
-import android.app.Application;
-import com.haanhgs.app.fragmentlistener.model.Model;
-import com.haanhgs.app.fragmentlistener.model.Repo;
-import com.haanhgs.app.fragmentlistener.model.Status;
-import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
 
-public class MyViewModel extends AndroidViewModel {
+public class MyViewModel extends ViewModel {
 
-    private Repo repo = new Repo();
 
-    public MyViewModel(@NonNull Application application) {
-        super(application);
-        repo.setLiveData();
-    }
-
-    public LiveData<Model> getData(){
-        return repo.getLiveData();
-    }
-
-    public void setStatus(Status status){
-        repo.setStatus(status);
-    }
-
-    public void toggleFragment(){
-        repo.toggleFragment();
-    }
 
 }
